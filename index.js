736e2d5cf5da8e33b8ce7d0d5407beb6641a6fc3
Adam Cox <73854883+AdamCox15@users.noreply.github.com>
2022-03-07 13:55:31 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+let word = "bottles";
+let count = 99;
+while (count > 0) {
+    if (count === 1){
+        word = "bottle"
+    }
+    console.log(count + " " + word + " of beer on the wall");
+    console.log(count + " " + word + " of beer,");
+    console.log("Take one down, pass it around,");
+
+    count = count - 1;
+
+    if (count > 0) {
+      if (count === 1){
+        console.log(count + " " + word.slice(0, -1) + " of beer on the wall.");
+      }
+    } else {
+        console.log("No more " + word + " of beer on the wall.");
+    }
+
+}
